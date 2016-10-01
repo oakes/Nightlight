93a7dafd0ecc2bf073f829c492c7dbd2e1f953c7
oakes <zsoakes@gmail.com>
2016-10-01 13:01:55 -0400

    Show editor and buttons

--- src/cljs/nightlight/core.cljs
+++ src/cljs/nightlight/core.cljs
@@ -1,9 +1,12 @@
 (ns nightlight.core
   (:require [paren-soup.core]
             [cljsjs.bootstrap]
-            [cljsjs.bootstrap-treeview]))
+            [cljsjs.bootstrap-treeview]
+            [paren-soup.core :as ps]))
 
 (.treeview (js/$ "#tree")
   (clj->js {:data [{:text "Parent 1"
                     :nodes [{:text "Child 1"}]}]}))
+
+(ps/init-all)
 
