03afe7f966636326cbc075e3cfe4323752fc3931
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-10-09 13:31:29 +0530

    add leiningen plugin

---
+++ lein-nightlight/project.clj
@@ -0,0 +1,10 @@
+(defproject nightlight/lein-nightlight "1.0.0"
+  :description "A conveninent Nightlight launcher for Leiningen projects"
+  :url "https://github.com/oakes/Nightlight"
+  :license {:name "Public domain"
+            :url "http://unlicense.org/UNLICENSE"}
+  :dependencies [[nightlight "1.0.0"]
+                 [leinjacker "0.4.2"]
+                 [org.clojure/tools.cli "0.3.5"]]
+  :eval-in-leiningen true)
+
