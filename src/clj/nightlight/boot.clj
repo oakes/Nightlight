9c7e779cb1251074d5b5d3a8b6966a2ff982a1c4
oakes <zsoakes@gmail.com>
2017-01-17 18:57:27 -0500

    Fix boot task

--- src/clj/nightlight/boot.clj
+++ src/clj/nightlight/boot.clj
@@ -11,7 +11,9 @@
    _ users USERS edn "A map of usernames and passwords to restrict access to"
    _ user USER str "A single username/password, separated by a space"]
   (core/with-pass-thru _
-    (start {:port port :url url :users (or users (apply hash-map (str/split user #" ")))})))
+    (start {:port port :url url :users (or users
+                                           (when user
+                                             (apply hash-map (str/split user #" "))))})))
 
 (def night nightlight)
 
