97a423a3ef4041dcd3c712e078de7a43fe5eb8da
oakes <zsoakes@gmail.com>
2017-07-16 17:13:25 -0400

    Increment template version number

--- lein-nightlight/project.clj
+++ lein-nightlight/project.clj
@@ -1,9 +1,9 @@
-(defproject nightlight/lein-nightlight "1.7.0"
+(defproject nightlight/lein-nightlight "1.7.1"
   :description "A conveninent Nightlight launcher for Leiningen projects"
   :url "https://github.com/oakes/Nightlight"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[nightlight "1.7.0" :exclusions [org.clojure/core.async]]
+  :dependencies [[nightlight "1.7.1" :exclusions [org.clojure/core.async]]
                  [leinjacker "0.4.2"]
                  [org.clojure/tools.cli "0.3.5"]]
   :eval-in-leiningen true)
