c35ab8bd010097418eb493eda39612a353cd9f2d
oakes <zach@sekao.net>
2017-10-14 13:28:39 -0400

    Increment version number

--- lein-nightlight/src/leiningen/nightlight.clj
+++ lein-nightlight/src/leiningen/nightlight.clj
@@ -26,7 +26,7 @@
   (eval/eval-in-project
     (deps/add-if-missing
       project
-      '[nightlight/lein-nightlight "1.9.0"])
+      '[nightlight/lein-nightlight "1.9.1"])
     `(do
        (nightlight.core/start
          {:port ~port :ip ~host :url ~url :users (or ~users ~user)})
