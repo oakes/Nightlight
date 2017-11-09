cdf4f0f3b7eb2f24aadd601a62b6c66e2fd5c8d8
oakes <zach@sekao.net>
2017-11-08 22:31:22 -0500

    Increment template version number

--- lein-nightlight/src/leiningen/nightlight.clj
+++ lein-nightlight/src/leiningen/nightlight.clj
@@ -26,7 +26,7 @@
   (eval/eval-in-project
     (deps/add-if-missing
       project
-      '[nightlight/lein-nightlight "1.9.2"])
+      '[nightlight/lein-nightlight "1.9.3"])
     `(do
        (nightlight.core/start
          {:port ~port :ip ~host :url ~url :users (or ~users ~user)})
