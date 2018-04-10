7a1d62548eba230c82fd6238606193a0fdfad967
oakes <zach@sekao.net>
2018-04-09 21:04:37 -0400

    Increment version number

--- lein-nightlight/src/leiningen/nightlight.clj
+++ lein-nightlight/src/leiningen/nightlight.clj
@@ -12,7 +12,7 @@
   (eval/eval-in-project
     (deps/add-if-missing
       project
-      '[nightlight/lein-nightlight "2.1.6"])
+      '[nightlight/lein-nightlight "2.1.7"])
     `(do
        (nightlight.core/start ~options)
        (when '~main (require '~main)))
