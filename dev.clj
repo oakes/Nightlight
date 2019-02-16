c3a8fdb1ad37c35c0b4c180cfe7f6c77cb7a0da7
oakes <zach@sekao.net>
2019-02-16 16:54:58 -0600

    Enable expound

--- dev.clj
+++ dev.clj
@@ -1,8 +1,11 @@
 (require
   '[orchestra.spec.test :as st]
+  '[expound.alpha :as expound]
+  '[clojure.spec.alpha :as s]
   '[figwheel.main :as figwheel]
   '[nightlight.core :as nightlight])
 
 (st/instrument)
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
 (nightlight/dev-start {:port 4000 :url "http://localhost:4000" :main 'nightlight.core})
 (figwheel/-main "--build" "dev")
--- src/nightlight/dev.cljs
+++ src/nightlight/dev.cljs
@@ -1,5 +1,8 @@
 (ns nightlight.dev
   (:require [nightlight.core]
-            [orchestra-cljs.spec.test :as st]))
+            [orchestra-cljs.spec.test :as st]
+            [expound.alpha :as expound]
+            [clojure.spec.alpha :as s]))
 
 (st/instrument)
+(set! s/*explain-out* expound/printer)
