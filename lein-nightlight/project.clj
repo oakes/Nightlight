9ca6a5edd6b0f09b355e02796aa61ff5c17747bd
paul.rutledge <paul.rutledge@vodori.com>
2019-05-11 01:41:37 -0500

    Upgrade leinjacker to version compatible with recent versions of leiningen

--- lein-nightlight/project.clj
+++ lein-nightlight/project.clj
@@ -1,10 +1,10 @@
 (defproject nightlight/lein-nightlight "2.4.1"
-  :description "A conveninent Nightlight launcher for Leiningen projects"
+  :description "A convenient Nightlight launcher for Leiningen projects"
   :url "https://github.com/oakes/Nightlight"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[nightlight "2.4.1" :exclusions [org.clojure/core.async]]
-                 [leinjacker "0.4.2"]
+                 [leinjacker "0.4.3"]
                  [org.clojure/tools.cli "0.3.5"]]
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
