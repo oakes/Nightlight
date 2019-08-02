6e0bca99740f8ffad335eeccd5e2a08cd309c293
oakes <zach@sekao.net>
2019-08-02 09:08:24 -0400

    Increment version number

--- lein-nightlight/project.clj
+++ lein-nightlight/project.clj
@@ -1,9 +1,9 @@
-(defproject nightlight/lein-nightlight "2.4.1.1"
+(defproject nightlight/lein-nightlight "2.4.2"
   :description "A convenient Nightlight launcher for Leiningen projects"
   :url "https://github.com/oakes/Nightlight"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[nightlight "2.4.1" :exclusions [org.clojure/core.async]]
+  :dependencies [[nightlight "2.4.2" :exclusions [org.clojure/core.async]]
                  [leinjacker "0.4.3"]
                  [org.clojure/tools.cli "0.3.5"]]
   :repositories [["clojars" {:url "https://clojars.org/repo"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nightlight "2.4.2-SNAPSHOT"
+(defproject nightlight "2.4.2"
   :description "An embedded Clojure editor"
   :url "https://github.com/oakes/Nightlight"
   :license {:name "Public Domain"
